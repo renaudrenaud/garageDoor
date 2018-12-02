c98884d24ffa961735b9443181ba09a9da22290d
renaudrenaud <renaud.renaud@gmail.com>
2018-12-02 21:42:21 +0100

    Working with bottle
    Let's open the door.

--- homeGate.py
+++ homeGate.py
@@ -1,15 +1,32 @@
-#
-# Garage Door
-#
-#
+from bottle import route, run, get, post, request, redirect
 
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 from time import sleep
+
 relay = port.PA6
 gpio.init()
 gpio.setcfg(relay, gpio.OUTPUT)
-sleep(1)
-gpio.output(relay, gpio.LOW)
-sleep(1)
 gpio.output(relay, gpio.HIGH)
+
+@route('/hello')
+def hello():
+    return "Hello World!"
+
+
+@get('/login') # or @route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            <input value="Open Door Garage" type="submit" />
+        </form>
+    '''
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    gpio.output(relay, gpio.LOW)
+    sleep(1)
+    gpio.output(relay, gpio.HIGH)
+    redirect('/login')
+
+run(host='192.168.1.103', port=8080, debug=True)
