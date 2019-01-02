5528f471eff270732792f6ec1b03a731bbd35ea4
renaudrenaud <renaud.renaud@gmail.com>
2019-01-02 11:56:40 +0100

    some html code to have a huge button

--- garageDoor.py
+++ garageDoor.py
@@ -5,35 +5,34 @@ from pyA20.gpio import port
 from time import sleep
 import socket
 
+# Define and initiate GPIO
 relay = port.PA6
 gpio.init()
 gpio.setcfg(relay, gpio.OUTPUT)
-gpio.output(relay, gpio.LOW)
+gpio.output(relay, gpio.LOW) # we start at LOW
 
+# Grab the IP address
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8",80))
 ipAddress = s.getsockname()[0]
 s.close()
 
-
-@route('/hello')
-def hello():
-    return "Hello World!"
-
-
-@get('/login') # or @route('/login')
+@get('/opendoor') #
 def login():
     return '''
         <form action="/login" method="post">
-            <input value="Open Door Garage" type="submit" />
+            <font size="10"><center>This is the open page!</center><hr>
+            <div title="hit the button to open">
+                <button style="text-align:center;font-size:80px" type="submit"> Open door </button>
+            </div /font>
         </form>
     '''
+@post('/opendoor') # 
 
-@post('/login') # or @route('/login', method='POST')
 def do_login():
     gpio.output(relay, gpio.HIGH)
     sleep(1)
     gpio.output(relay, gpio.LOW)
     redirect('/login')
 
-run(host=ipAddress, port=8080, debug=True)
+run(host=ipAddress, port=8080, debug=True) # we use thez IP address grabbed before and port 8080
